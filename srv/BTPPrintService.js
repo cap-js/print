946aa3175261d1583a0a607f35f6da919d13ed06
I569192 <simon.engel01@sap.com>
2025-12-15 19:10:43 +0100

    refactoring to use cache and utils

--- srv/BTPPrintService.js
+++ srv/BTPPrintService.js
@@ -1,108 +1,26 @@
 const PrintService = require("./service");
 const cds = require("@sap/cds");
 const LOG = cds.log("print");
-const {
-  getDestinationFromServiceBinding,
-  transformServiceBindingToClientCredentialsDestination,
-} = require("@sap-cloud-sdk/connectivity");
-const { executeHttpRequest, executeHttpRequestWithOrigin } = require("@sap-cloud-sdk/http-client");
+const { getServiceToken, getServiceCredentials } = require("../lib/btp-utils");
+const { TokenCache } = require("../lib/TokenCache");
 
 module.exports = class BTPPrintService extends PrintService {
+  tokenCache;
+
   async init() {
     LOG.info("Productive Print service initialized.");
     return super.init();
-  }
-
-  _getServiceCredentials(serviceName) {
-    const vcap = JSON.parse(process.env["VCAP_SERVICES"] || "{}");
-    return (vcap[serviceName] || [])[0]?.credentials;
-  }
-
-  async _getToken(authUrl, clientId, clientSecret, isConsumerSpecific, tenantId) {
-    try {
-      // The header attribute 'X-Zid' is set to the consumer tenant id, this retrieves a consumer-specific JWT (if requested)
-      const authorization = Buffer.from(`${clientId}:${clientSecret}`).toString("base64");
-      const response = await executeHttpRequest(
-        {
-          url: `${authUrl}/oauth/token?grant_type=client_credentials`,
-        },
-        {
-          method: "GET",
-          headers: {
-            Authorization: `Basic ${authorization}`,
-            ...(isConsumerSpecific && { "X-Zid": tenantId }),
-          },
-        },
-      );
-      return response;
-    } catch (error) {
-      console.error("Error receiving JWT token:", error.message);
-    }
-  }
-
-  async getServiceToken(serviceName, inTenantId, isConsumerSpecific = true) {
-    const srvCredentials = this._getServiceCredentials(serviceName);
-    if (!srvCredentials) {
-      console.error(`Missing binding credentials for service "${serviceName}"`);
-      throw new Error(`Missing binding credentials for service "${serviceName}"`);
-    }
-
-    const tenantId = inTenantId || cds.context?.tenant;
-    const clientId = srvCredentials?.uaa?.clientid || srvCredentials?.clientid;
-    const clientSecret = srvCredentials?.uaa?.clientsecret || srvCredentials?.clientsecret;
-    const authUrl = srvCredentials?.uaa?.url || srvCredentials?.url;
-
-    if (isConsumerSpecific && !tenantId) {
-      console.error(
-        "Util ServiceCredentials - getServiceToken: tenantId missing for consumer-specific token request",
-      );
-      throw new Error(
-        `Tenant ID missing during token retrieval for bound service "${serviceName}"`,
-      );
-    }
-
-    // Security check: A consumer tenant must not receive the token of another consumer tenant
-    if (tenantId) {
-      const providerTenantID = this._getServiceCredentials("xsuaa")?.tenantid;
-      let currentTenantID = cds.context?.tenant;
-      if (providerTenantID !== currentTenantID && currentTenantID !== tenantId) {
-        const errorMessage =
-          "Util ServiceCredentials - getServiceToken: action not allowed for consumer tenant";
-        console.error(errorMessage);
-        throw new Error(`Consumer Tenant foreign token retrieval not allowed "${serviceName}"`);
-      }
-    }
-
-    const responseGetToken = await this._getToken(
-      authUrl,
-      clientId,
-      clientSecret,
-      isConsumerSpecific,
-      tenantId,
-    );
-    const jwt = responseGetToken?.data?.access_token;
-
-    if (!jwt) {
-      console.error(
-        "Util ServiceCredentials - getServiceToken: empty token from service",
-        serviceName,
-      );
-      throw new Error(
-        `Empty JWT returned from authorization service for bound service "${serviceName}"`,
-      );
-    }
-
-    return jwt;
+    tokenCache = new TokenCache();
   }
 
   /**
    * Get available print queues
    */
   async getQueues() {
-    const srvUrl = this._getServiceCredentials("print")?.service_url;
+    const srvUrl = getServiceCredentials("print")?.service_url;
     let jwt = "";
     try {
-      jwt = await this.getServiceToken("print");
+      jwt = await this.getToken(cds.context?.tenant);
     } catch (e) {
       console.error("ACTION print: Error retrieving jwt", e.message);
       throw new Error(httpCodes.internal_server_error, "ACTION_PRINT_NO_ACCESS");
@@ -148,14 +66,15 @@ module.exports = class BTPPrintService extends PrintService {
    * @param {Object} req - The request object.
    */
   async _print(printRequest) {
+    const tenantId = cds.context?.tenant;
     const { qname: selectedQueue, numberOfCopies, docsToPrint } = printRequest;
 
     cds.log("=== REQUEST BASIC INFO ===");
-    const srvUrl = this._getServiceCredentials("print")?.service_url;
+    const srvUrl = getServiceCredentials("print")?.service_url;
 
     let jwt = "";
     try {
-      jwt = await this.getServiceToken("print");
+      jwt = await this.getToken(tenantId);
     } catch (e) {
       LOG.error("ACTION print: Error retrieving jwt", e.message);
       throw new Error("No access to print service.");
@@ -216,7 +135,6 @@ module.exports = class BTPPrintService extends PrintService {
         },
       };
 
-      const tenantId = cds.context?.tenant;
       const r = await fetch(`${srvUrl}/qm/api/v1/rest/print-tasks/${itemId}`, {
         method: "PUT",
         headers: {
@@ -227,6 +145,7 @@ module.exports = class BTPPrintService extends PrintService {
         },
         body: JSON.stringify(data),
       });
+
       if (!r.ok) {
         const body = await r.text();
         console.error("Print task creation failed. Response body:", body);
@@ -252,5 +171,17 @@ module.exports = class BTPPrintService extends PrintService {
     return {
       taskId: itemId,
     };
+  }
+
+  async getToken(tenantId) {
+    const tokenFromCache = this.tokenCache.get?.(tenantId);
+    return (
+      tokenFromCache ??
+      (await (async () => {
+        const [jwtFromService, expires_in] = await getServiceToken("print");
+        this.tokenCache.set?.(tenantId, jwtFromService, expires_in);
+        return jwtFromService;
+      })())
+    );
   }
 };
