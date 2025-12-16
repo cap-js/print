5fc0bb430d70658723c189f684da871fbed618fe
I569192 <simon.engel01@sap.com>
2025-12-16 19:05:07 +0100

    update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,21 @@ export default [
   ...cds,
   {
     files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        SELECT: "readonly",
+        INSERT: "readonly",
+        UPSERT: "readonly",
+        UPDATE: "readonly",
+        DELETE: "readonly",
+        CREATE: "readonly",
+        DROP: "readonly",
+        CDL: "readonly",
+        CQL: "readonly",
+        CXL: "readonly",
+        cds: "readonly",
+      },
+    },
     rules: {
       "no-await-in-loop": "error",
       "no-console": ["error", { allow: ["warn", "error"] }],
